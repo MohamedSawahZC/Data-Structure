3a2c66bf12a6d2be76141bc1f929f3019c484638
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2022-12-01 09:46:54 +0200

    Circular queue using static array

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1>Data Strucuture</h1>
+
+<img src="Images/Banner.png" />
+
+<h2 style="color:#FF7000;">Introduction</h2>
+
+- This a data structure implementation that will help student to understand data structure with perfect comments in every code file
+
+<h3 style="color:crimson;">What is data structure?</h3>
+
+<h4><div style="color:lightgreen;">Data Structure:</div> is a specialized format for organizing, processing, retrieving and storing data. </h4>
+
+<h2 style="color:#FF7000;">Topics</h2>
+
+<ul style="color:#10A19D; font-weight:bold; font-size:20px"> 
+<li>Stack with array</li>
+<li>Stack with linked list</li>
+<li>balanced parenthesis problem using stack</li>
+<li>Infix To Postfix using stack</li>
+<li>Queue using circular array</li>
+<li>Queue using linked list</li>
+<li>Array based list</li>
+</ul>
---
+++ circular queue using static array.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+/*
+ ================================Queue ================================
+ Queue work with FIFO (First in First out)
+ Real Examples:
+ 1- Cinema Queue
+ 2- Clinic Reservation
+ 3- Printer
+ 4- Call Center System
+ 5- CPU Scheduling in Operating System [Task 1 , Task 2 , Task 2]
+ In Queue:
+  ~ We add in the back
+  ~ Delete from the head
+Note:
+rear initial value = MAX_SIZE -1;
+rear = (rear+1) % MAX_SIZE;
+Elements between front,rear this is the queue range
+Methods:
+    isEmpty() : For check if queue is isEmpty
+    isFull() : For check if queue is full
+    enqueue(): To add element to queue
+    dequeue(): To delete element (pop) from the queue
+    getFront(): To get the front
+    getRear(): To get the rear
+    getSize(): To get stack size
+    clear(): To clear the queue
+ ======================================================================
+*/
+const int MAX_SIZE = 100;
+class arrayQueueType{
+private:
+    int rear; //you can call it back
+    int front; //you can call it head
+    int length;
+    int arr[MAX_SIZE];
+public:
+    arrayQueueType(){
+        front=0; //First element in the queue
+        rear=MAX_SIZE-1; //100-1 = 99 end of the queue
+        length=0;
+    }
+//@desc Method To check if the queue is isEmpty
+int isEmpty()
+	{
+		return length == 0;
+	}
+//@desc Method  To check if the queue full
+bool isFull(){
+    return length==MAX_SIZE;
+}
+//@desc Method to add to queue
+void addQueue(int element){
+    if(isFull()){
+        cout<<"Queue full cannot Enqueu..."<<endl;
+    }else{
+        rear = (rear + 1) % MAX_SIZE;
+			arr[rear] = element;
+			length++;
+    }
+}
+//@desc Method to delele from the queue
+void deleteQueue(){
+    if(isEmpty()){
+        cout<<"Empty queue cannot dequeue..."<<endl;
+    }else{
+        front = (front+1)%MAX_SIZE;
+        --length;
+    }
+}
+
+//@desc Method to get front from the queue
+    int frontQueue(){
+       	assert(!isEmpty());
+		return arr[front];
+    }
+//@desc Method to get rear from the queue
+    int rearQueue(){
+        assert(!isEmpty()); //assert stop the program if there is any problem
+        return arr[rear];
+    }
+//@desc Method to get front in variable
+void getFront(int&getFront){
+    getFront = arr[front];
+}
+//@desc Method to print Queue elements
+void printQueue(){
+    if(isEmpty()){
+        cout<<"Sorry empty Queue"<<endl;
+    }else{
+        cout<<"[ ";
+    for(size_t i = front; i != rear; i = (i + 1) % MAX_SIZE){
+        cout<<arr[i]<<" ";
+    }
+    cout << arr[rear];
+    cout<<" ]"<<endl;
+    }
+}
+
+
+
+};
+
+
+int main() {
+    arrayQueueType q1;
+    q1.printQueue();
+    q1.addQueue(10);
+    q1.addQueue(12);
+    q1.addQueue(20);
+    q1.addQueue(30);
+    q1.printQueue();
+    cout<<q1.frontQueue()<<endl;
+    q1.deleteQueue();
+    q1.printQueue();
+    cout<<q1.frontQueue()<<endl;
+    return 0;
+}
